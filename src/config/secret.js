fcbe9c31a2e6e757b316df6a56e1f789f2ed08c4
delta898 <delta898@gmail.com>
2026-01-27 22:44:43 +0900

    add src/config/secret.js

---
+++ src/config/secret.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // 실제 운영 서버 주소 (하드코딩)
+    LICENSE_CHK_URL: 'https://hocfjolcthvtgfaxjmse.supabase.co',
+    LICENSE_CHK_KEY: 'sb_publishable_4DObqEWTXyZNRlmJdB3bqQ_BstyktIJ',
+};
