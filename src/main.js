785ec21d2840e808e44fee9b25d8b7dfeae75529
delta898 <delta898@gmail.com>
2026-02-13 23:38:29 +0900

    chore: release assets workflow and bump v0.6.1

--- src/main.js
+++ src/main.js
@@ -69,8 +69,8 @@ const program = new Command();
 program
     .name('BlogGenius')
     .usage('[command] [options]')
-    .version('0.6.0')
-    .description('🤖 네이버 블로그 자동 포스팅 봇 - Topic 기반 엔진 (v0.6.0)');
+    .version('0.6.1')
+    .description('🤖 네이버 블로그 자동 포스팅 봇 - Topic 기반 엔진 (v0.6.1)');
 
 // --- Helper Functions ---
 
